850f7692cb61ebef22f19798c5ad193a657e5c70
shadowM <shadowM@shadowM.local>
2017-03-30 15:03:36 -0500

    include mpi improvements

--- open-mpi/pingPong.c
+++ open-mpi/pingPong.c
@@ -5,21 +5,21 @@
 int main(int argc, char** argv) {
   const int limit = 10;
   MPI_Init(NULL, NULL);
-  int rank; //obtener ID del proceso
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  int size; //numero de procesos
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int idProcess; //obtener ID del proceso
+  MPI_Comm_rank(MPI_COMM_WORLD, &idProcess);
+  int quantity; //numero de procesos
+  MPI_Comm_size(MPI_COMM_WORLD, &quantity);
 
-  int contadorPingPong = 0;
-  int rankPart = (rank + 1) % 2;
-  while (contadorPingPong < limit) {
-    if (rank == contadorPingPong % 2) { 
-      contadorPingPong++;
-      MPI_Send(&contadorPingPong, 1, MPI_INT, rankPart, 0, MPI_COMM_WORLD);
-      printf("%d envia a destino  :%d   con contador:%d \n", rank, rankPart, contadorPingPong);
+  int cont = 0;
+  int enemy = (idProcess + 1) % 2;
+  while (cont < limit) {
+    if (idProcess == cont % 2) { 
+      cont++;
+      MPI_Send(&cont, 1, MPI_INT, enemy, 0, MPI_COMM_WORLD);
+      printf("%d envia a destino  :%d   con contador:%d \n", idProcess, enemy, contador);
     } else {
-      MPI_Recv(&contadorPingPong, 1, MPI_INT, rankPart, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      printf("%d recibe de origen :%d   con contador:%d \n", rank, rankPart, contadorPingPong);
+      MPI_Recv(&cont, 1, MPI_INT, enemy, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("%d recibe de origen :%d   con contador:%d \n", idProcess, enemy, contador);
     }
   }
   MPI_Finalize();
