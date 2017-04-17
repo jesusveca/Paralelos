f27cf5c50684435e843cfdba1d0acc88e8448c75
shadowM <shadowM@shadowM.local>
2017-04-17 14:16:23 -0500

    add linked list

--- pthread/lista_enlazada_pth.c
+++ pthread/lista_enlazada_pth.c
@@ -1,140 +1,39 @@
-/*
- * Purpose:  Implement a multi-threaded sorted linked list of 
- *           ints with ops insert, print, member, delete, free list.  
- *           This version uses a single mutex
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include "my_rand.h"
 #include "timer.h"
 
-/* Random ints are less than MAX_KEY */
 const int MAX_KEY = 100000000;
 
-/* Struct for list nodes */
 struct list_node_s {
    int    data;
    struct list_node_s* next;
 };
 
-/* Shared variables */
 struct      list_node_s* head = NULL;  
-int         thread_count;
-int         total_ops;
-double      insert_percent;
-double      search_percent;
-double      delete_percent;
+int         thread_count;           int         total_ops;
+double      insert_percent;         double      search_percent;         double      delete_percent;
 pthread_mutex_t mutex;
 pthread_mutex_t count_mutex;
-int         member_total=0, insert_total=0, delete_total=0;
+int member_total=0, insert_total=0, delete_total=0;
 
-/* Setup and cleanup */
-void        Usage(char* prog_name);
-void        Get_input(int* inserts_in_main_p);
-
-/* Thread function */
-void*       Thread_work(void* rank);
-
-/* List operations */
-int         Insert(int value);
-void        Print(void);
-int         Member(int value);
-int         Delete(int value);
-void        Free_list(void);
-int         Is_empty(void);
-
-/*-----------------------------------------------------------------*/
-int main(int argc, char* argv[]) {
-   long i; 
-   int key, success, attempts;
-   pthread_t* thread_handles;
-   int inserts_in_main;
-   unsigned seed = 1;
-   double start, finish;
-
-   if (argc != 2) Usage(argv[0]);
-   thread_count = strtol(argv[1],NULL,10);
-
-   Get_input(&inserts_in_main);
-
-   /* Try to insert inserts_in_main keys, but give up after */
-   /* 2*inserts_in_main attempts.                           */
-   i = attempts = 0;
-   while ( i < inserts_in_main && attempts < 2*inserts_in_main ) {
-      key = my_rand(&seed) % MAX_KEY;
-      success = Insert(key);
-      attempts++;
-      if (success) i++;
-   }
-   printf("Inserted %ld keys in empty list\n", i);
-
-#  ifdef OUTPUT
-   printf("Before starting threads, list = \n");
-   Print();
-   printf("\n");
-#  endif
-
-   thread_handles = malloc(thread_count*sizeof(pthread_t));
-   pthread_mutex_init(&mutex, NULL);
-   pthread_mutex_init(&count_mutex, NULL);
-
-   GET_TIME(start);
-   for (i = 0; i < thread_count; i++)
-      pthread_create(&thread_handles[i], NULL, Thread_work, (void*) i);
-
-   for (i = 0; i < thread_count; i++)
-      pthread_join(thread_handles[i], NULL);
-   GET_TIME(finish);
-   printf("Elapsed time = %e seconds\n", finish - start);
-   printf("Total ops = %d\n", total_ops);
-   printf("member ops = %d\n", member_total);
-   printf("insert ops = %d\n", insert_total);
-   printf("delete ops = %d\n", delete_total);
-
-#  ifdef OUTPUT
-   printf("After threads terminate, list = \n");
-   Print();
-   printf("\n");
-#  endif
-
-   Free_list();
-   pthread_mutex_destroy(&mutex);
-   pthread_mutex_destroy(&count_mutex);
-   free(thread_handles);
-
-   return 0;
-}  /* main */
-
-
-/*-----------------------------------------------------------------*/
-void Usage(char* prog_name) {
-   fprintf(stderr, "usage: %s <thread_count>\n", prog_name);
-   exit(0);
-}  /* Usage */
-
-/*-----------------------------------------------------------------*/
 void Get_input(int* inserts_in_main_p) {
-
-   printf("How many keys should be inserted in the main thread?\n");
+   printf("Cuantas inserciones desea hacer ? \n");
    scanf("%d", inserts_in_main_p);
-   printf("How many total ops should be executed?\n");
+   printf("Cuantas operacones en total desea hacer? \n");
    scanf("%d", &total_ops);
-   printf("Percent of ops that should be searches? (between 0 and 1)\n");
+   printf("Porcetaje para busquedas: \n");
    scanf("%lf", &search_percent);
-   printf("Percent of ops that should be inserts? (between 0 and 1)\n");
+   printf("Porcentajes de inserciones: \n");
    scanf("%lf", &insert_percent);
    delete_percent = 1.0 - (search_percent + insert_percent);
-}  /* Get_input */
+} 
 
-/*-----------------------------------------------------------------*/
-/* Insert value in correct numerical location into list */
-/* If value is not in list, return 1, else return 0 */
 int Insert(int value) {
    struct list_node_s* curr = head;
    struct list_node_s* pred = NULL;
    struct list_node_s* temp;
-   int rv = 1;
    
    while (curr != NULL && curr->data < value) {
       pred = curr;
@@ -149,115 +48,46 @@ int Insert(int value) {
          head = temp;
       else
          pred->next = temp;
-   } else { /* value in list */
-      rv = 0;
+      return 1;
+   } else { 
+      return 0;
    }
+} 
 
-   return rv;
-}  /* Insert */
-
-/*-----------------------------------------------------------------*/
-void Print(void) {
-   struct list_node_s* temp;
-
-   printf("list = ");
-
-   temp = head;
-   while (temp != (struct list_node_s*) NULL) {
-      printf("%d ", temp->data);
-      temp = temp->next;
-   }
-   printf("\n");
-}  /* Print */
-
-
-/*-----------------------------------------------------------------*/
 int  Member(int value) {
    struct list_node_s* temp;
-
    temp = head;
    while (temp != NULL && temp->data < value)
       temp = temp->next;
-
    if (temp == NULL || temp->data > value) {
-#     ifdef DEBUG
-      printf("%d is not in the list\n", value);
-#     endif
       return 0;
    } else {
-#     ifdef DEBUG
-      printf("%d is in the list\n", value);
-#     endif
       return 1;
    }
-}  /* Member */
+}  
 
-/*-----------------------------------------------------------------*/
-/* Deletes value from list */
-/* If value is in list, return 1, else return 0 */
 int Delete(int value) {
    struct list_node_s* curr = head;
    struct list_node_s* pred = NULL;
-   int rv = 1;
 
-   /* Find value */
    while (curr != NULL && curr->data < value) {
       pred = curr;
       curr = curr->next;
    }
-   
    if (curr != NULL && curr->data == value) {
-      if (pred == NULL) { /* first element in list */
+      if (pred == NULL) { 
          head = curr->next;
-#        ifdef DEBUG
-         printf("Freeing %d\n", value);
-#        endif
          free(curr);
       } else { 
          pred->next = curr->next;
-#        ifdef DEBUG
-         printf("Freeing %d\n", value);
-#        endif
          free(curr);
       }
-   } else { /* Not in list */
-      rv = 0;
-   }
-
-   return rv;
-}  /* Delete */
-
-/*-----------------------------------------------------------------*/
-void Free_list(void) {
-   struct list_node_s* current;
-   struct list_node_s* following;
-
-   if (Is_empty()) return;
-   current = head; 
-   following = current->next;
-   while (following != NULL) {
-#     ifdef DEBUG
-      printf("Freeing %d\n", current->data);
-#     endif
-      free(current);
-      current = following;
-      following = current->next;
-   }
-#  ifdef DEBUG
-   printf("Freeing %d\n", current->data);
-#  endif
-   free(current);
-}  /* Free_list */
-
-/*-----------------------------------------------------------------*/
-int  Is_empty(void) {
-   if (head == NULL)
       return 1;
-   else
+   } else { 
       return 0;
-}  /* Is_empty */
+   }
+}  
 
-/*-----------------------------------------------------------------*/
 void* Thread_work(void* rank) {
    long my_rank = (long) rank;
    int i, val;
@@ -279,13 +109,13 @@ void* Thread_work(void* rank) {
          Insert(val);
          pthread_mutex_unlock(&mutex);
          my_insert++;
-      } else { /* delete */
+      } else { 
          pthread_mutex_lock(&mutex);
          Delete(val);
          pthread_mutex_unlock(&mutex);
          my_delete++;
       }
-   }  /* for */
+   }  
 
    pthread_mutex_lock(&count_mutex);
    member_total += my_member;
@@ -294,4 +124,47 @@ void* Thread_work(void* rank) {
    pthread_mutex_unlock(&count_mutex);
 
    return NULL;
-}  /* Thread_work */
+}  
+
+int main(int argc, char* argv[]) {
+   long i; 
+   int key, success, attempts;
+   pthread_t* thread_handles;
+   int inserts_in_main;
+   unsigned seed = 1;
+   double start, finish;
+
+   thread_count = strtol(argv[1],NULL,10);
+
+   Get_input(&inserts_in_main);
+
+   i = attempts = 0;
+   while ( i < inserts_in_main && attempts < 2*inserts_in_main ) {
+      key = my_rand(&seed) % MAX_KEY;
+      success = Insert(key);
+      attempts++;
+      if (success) i++;
+   }
+   printf("Inserto %ld nodos en lista\n", i);
+
+   thread_handles = malloc(thread_count*sizeof(pthread_t));
+   pthread_mutex_init(&mutex, NULL);
+   pthread_mutex_init(&count_mutex, NULL);
+
+   GET_TIME(start);
+   for (i = 0; i < thread_count; i++)
+      pthread_create(&thread_handles[i], NULL, Thread_work, (void*) i);
+   for (i = 0; i < thread_count; i++)
+      pthread_join(thread_handles[i], NULL);
+   GET_TIME(finish);
+   printf("Tiempo Transcurrido = %e seconds\n", finish - start);
+   printf("Operaciones Totales = %d\n", total_ops);
+   printf("Ops Busqueda = %d\n", member_total);
+   printf("Ops Inserciones = %d\n", insert_total);
+   printf("Ops Borrado= %d\n", delete_total);
+
+   pthread_mutex_destroy(&mutex);
+   pthread_mutex_destroy(&count_mutex);
+   free(thread_handles);
+   return 0;
+}  
